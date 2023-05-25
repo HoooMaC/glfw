be8b287b94960bab98ff1eaecb4c19e6f9878ea2
HoooMaC <118969706+HoooMaC@users.noreply.github.com>
2023-05-25 21:25:36 +0700

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,45 @@
+project "GLFW"
+	kind "StaticLib"
+	language "C"
+  staticruntime "On"
+  systemversion "latest"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+		"src/vulkan.c",
+		"src/window.c"
+    "src/x11_init.c",
+    "src/x11_monitor.c",
+    "src/x11_window.c",
+    "src/xkb_unicode.c",
+    "src/posix_time.c",
+    "src/posix_thread.c",
+    "src/glx_context.c",
+    "src/egl_context.c",
+    "src/osmesa_context.c",
+    "src/linux_joystick.c"
+  }
+
+  defines 
+  { 
+    "_GLFW_WIN32",
+    "_CRT_SECURE_NO_WARNINGS"
+  }
+
+filter "configurations:Debug"
+  runtime "Debug"
+  symbols "on"
+
+filter "configurations:Release"
+  runtime "Release"
+  optimize "on"
