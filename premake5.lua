8d8c2525aa4cd2074174f133349ef4bd72856fe3
HoooMaC <wawcam123@gmail.com>
2023-05-25 23:37:03 +0700

    merapihkan premake dan ignore outputdir

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,8 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-  staticruntime "On"
-  systemversion "latest"
+	staticruntime "On"
+	systemversion "latest"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -17,29 +17,29 @@ project "GLFW"
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
-		"src/window.c"
-    "src/x11_init.c",
-    "src/x11_monitor.c",
-    "src/x11_window.c",
-    "src/xkb_unicode.c",
-    "src/posix_time.c",
-    "src/posix_thread.c",
-    "src/glx_context.c",
-    "src/egl_context.c",
-    "src/osmesa_context.c",
-    "src/linux_joystick.c"
-  }
+		"src/window.c",
+		"src/x11_init.c",
+		"src/x11_monitor.c",
+		"src/x11_window.c",
+		"src/xkb_unicode.c",
+		"src/posix_time.c",
+		"src/posix_thread.c",
+		"src/glx_context.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/linux_joystick.c"
+	}
 
-  defines 
-  { 
-    "_GLFW_WIN32",
-    "_CRT_SECURE_NO_WARNINGS"
-  }
+	defines 
+	{ 
+		"_GLFW_WIN32",
+		"_CRT_SECURE_NO_WARNINGS"
+	}
 
-filter "configurations:Debug"
-  runtime "Debug"
-  symbols "on"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
 
-filter "configurations:Release"
-  runtime "Release"
-  optimize "on"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
