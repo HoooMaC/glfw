94cdad8c562e4ebcf620deaef99837af5231af19
HoooMaC <wawcam123@gmail.com>
2023-08-27 16:06:08 +0700

    edit premake

--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,19 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "On"
+	staticruntime "on"
 	systemversion "latest"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "-%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "-%{prj.name}")
 
 	files
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+		"src/**.h",
 		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c",
-		"src/x11_init.c",
-		"src/x11_monitor.c",
-		"src/x11_window.c",
-		"src/xkb_unicode.c",
-		"src/posix_time.c",
-		"src/posix_thread.c",
-		"src/glx_context.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c",
-		"src/linux_joystick.c"
+		"src/**.c"
 	}
 
 	defines 
@@ -37,9 +23,11 @@ project "GLFW"
 	}
 
 	filter "configurations:Debug"
+		defines { "_DEBUG" }
 		runtime "Debug"
 		symbols "on"
 
 	filter "configurations:Release"
+		defines { "NDEBUG" }
 		runtime "Release"
 		optimize "on"
